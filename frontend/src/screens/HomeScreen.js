99b1e7c835af7b21d9adab2f375a5b2b387ec23d
Vaibhav Kambli <vaibhavk2608@gmail.com>
2020-12-02 14:22:16 -0500

    Added Axios for fetching data from the backend server

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
-import products from "../products";
+import axios from "axios";
 
 const HomeScreen = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const { data } = await axios.get("/api/products");
+
+			setProducts(data);
+		};
+
+		fetchProducts();
+	}, []);
+
 	return (
 		<>
-			<h1>latest</h1>
+			<h1>Latest Products</h1>
 
 			<Row>
 				{products.map((product) => (
