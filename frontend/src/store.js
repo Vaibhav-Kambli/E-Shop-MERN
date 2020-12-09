3bc25ed038c02ea6e7c9ba77c76dbeed4a9cf087
Vaibhav Kambli <vaibhavk2608@gmail.com>
2020-12-09 14:41:03 -0500

    Added User Register Reducer, Action and Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,13 +6,14 @@ import {
 	productDetailsReducer,
 } from "./reducers/productReducers.js";
 import { cartReducer } from "./reducers/cartReducers.js";
-import { userLoginReducer } from "./reducers/userReducers.js";
+import { userLoginReducer, userRegisterReducer} from "./reducers/userReducers.js";
 
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
 	cart: cartReducer,
 	userLogin: userLoginReducer,
+	userRegister: userRegisterReducer
 });
 
 // get cart item data from localstorage and parse it else `cartItemsFromStorage = []`
