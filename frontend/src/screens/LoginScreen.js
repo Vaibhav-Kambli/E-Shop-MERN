f0e9c9b90768d52ee2abeef7cdfaad5c766bb8e2
Vaibhav Kambli <vaibhavk2608@gmail.com>
2020-12-09 14:39:46 -0500

    Fixed redirect variable split typo

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -11,7 +11,7 @@ const LoginScreen = ({location, history}) => {
 	const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     
-    const redirect = location.search ? location.search.spilit('=')[1] : '/'
+    const redirect = location.search ? location.search.split('=')[1] : '/'
 
     const dispatch = useDispatch()
 
