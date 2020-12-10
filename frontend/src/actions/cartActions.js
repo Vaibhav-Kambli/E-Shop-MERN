21dfb3fd4d1be5cd4aaf2415ec935a7134e54b45
Vaibhav Kambli <vaibhavk2608@gmail.com>
2020-12-10 15:43:02 -0500

    Added Payment Method Screen, Constant, Reducer, Action and State

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants'
 import axios from 'axios'
 
 export const addToCart =(id, quantity) => async (dispatch, getState) => {
@@ -38,5 +38,15 @@ export const saveShippingAddress =  (data) => (dispatch) => {
     })
 
     localStorage.setItem('shippingAddress', JSON.stringify(data))
+
+}
+
+export const savePaymentMethod =  (data) => (dispatch) => {
+    dispatch({
+        type: CART_SAVE_PAYMENT_METHOD,
+        payload: data
+    })
+
+    localStorage.setItem('paymentMethod', JSON.stringify(data))
 
 }
