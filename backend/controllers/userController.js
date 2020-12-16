b22a566ecbcd8c54dab0b967bde75a6034002f04
Vaibhav Kambli <vaibhavk2608@gmail.com>
2020-12-16 18:42:18 -0500

    Added Get User By ID and Update User by ID Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -145,7 +145,51 @@ const deleteUser = AsyncHandler(async (req, res) => {
 	
 });
 
+/**
+ * @description Get user by id
+ * @route       GET /api/users/:id
+ * @access      Private/Admin
+ */
+const getUserById = AsyncHandler(async (req, res) => {
+	const user = await User.findById(req.params.id).select('-password');
+	if(user) {
+		res.json(user)
+	}else {
+		res.status(404)
+		throw new Error('User not found')
+	}	
+});
+
+/**
+ * @description Update user profile by id
+ * @route       PUT /api/users/:id
+ * @access      Private/Admin
+ */
+const updateUser = AsyncHandler(async (req, res) => {
+	const user = await User.findById(req.params.id);
+
+	if (user) {
+		user.name = req.body.name || user.name;
+		user.email = req.body.email || user.email;
+		user.isAdmin = Boolean(req.body.isAdmin)
+
+		const updatedUser = await user.save()
+
+		res.json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+		});
+		
+	} else {
+		res.status(404);
+		throw new Error("User not found");
+	}
+});
 
 
 
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser};
+
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser};
