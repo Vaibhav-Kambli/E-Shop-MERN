05cd34aa03af70b21106353dbceba444a87aef85
Vaibhav Kambli <vaibhavk2608@gmail.com>
2020-12-15 20:11:13 -0500

    Added Admin User List Screen, Action, Reducer and Constant

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,6 +3,9 @@ import {
 	USER_DETAILS_REQUEST,
 	USER_DETAILS_RESET,
 	USER_DETAILS_SUCCESS,
+	USER_LIST_FAIL,
+	USER_LIST_REQUEST,
+	USER_LIST_SUCCESS,
 	USER_LOGIN_FAIL,
 	USER_LOGIN_REQUEST,
 	USER_LOGIN_SUCCESS,
@@ -102,7 +105,7 @@ export const userUpdateProfileReducer = (state = {}, action) => {
 	switch (action.type) {
 		case USER_UPDATE_PROFILE_REQUEST:
 			return {
-				...state,
+				//...state,
 				loading: true,
 			};
 
@@ -110,7 +113,7 @@ export const userUpdateProfileReducer = (state = {}, action) => {
 			return {
 				loading: false,
 				success: true,
-				user: action.payload,
+				userInfo: action.payload,
 			};
 
 		case USER_UPDATE_PROFILE_FAIL:
@@ -122,6 +125,30 @@ export const userUpdateProfileReducer = (state = {}, action) => {
 		case USER_UPDATE_PROFILE_RESET:
 			return {}
 
+
+		default:
+			return state;
+	}
+};
+
+export const userListReducer = (state = { users: [] }, action) => {
+	switch (action.type) {
+		case USER_LIST_REQUEST:
+			return {
+				loading: true,
+			};
+
+		case USER_LIST_SUCCESS:
+			return {
+				loading: false,
+				users: action.payload,
+			};
+
+		case USER_LIST_FAIL:
+			return {
+				loading: false,
+				error: action.payload,
+			};
 
 		default:
 			return state;
