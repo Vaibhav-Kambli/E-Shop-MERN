702643172fd73b81251824903882ad899d17508e
Vaibhav Kambli <vaibhavk2608@gmail.com>
2020-12-17 20:15:00 -0500

    Added Product Create Functionality

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,51 +1,62 @@
-import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL} from '../constants/productConstants.js'
-import axios from 'axios'
+import {
+	PRODUCT_LIST_REQUEST,
+	PRODUCT_LIST_SUCCESS,
+	PRODUCT_LIST_FAIL,
+	PRODUCT_DETAILS_REQUEST,
+	PRODUCT_DETAILS_SUCCESS,
+	PRODUCT_DETAILS_FAIL,
+	PRODUCT_DELETE_REQUEST,
+	PRODUCT_DELETE_SUCCESS,
+	PRODUCT_DELETE_FAIL,
+	PRODUCT_CREATE_SUCCESS,
+	PRODUCT_CREATE_FAIL,
+	PRODUCT_CREATE_REQUEST,
+} from "../constants/productConstants.js";
+import axios from "axios";
 
+export const listProducts = () => async dispatch => {
+	try {
+		dispatch({ type: PRODUCT_LIST_REQUEST });
 
-export const listProducts =  () => async(dispatch) => {
-    try{
-        dispatch({ type: PRODUCT_LIST_REQUEST})
+		const { data } = await axios.get("/api/products");
 
+		dispatch({
+			type: PRODUCT_LIST_SUCCESS,
+			payload: data,
+		});
+	} catch (err) {
+		dispatch({
+			type: PRODUCT_LIST_FAIL,
+			payload:
+				err.response && err.response.data.message
+					? err.response.data.message
+					: err.message,
+		});
+	}
+};
 
-        const {data} = await axios.get('/api/products')
+export const listProductDetails = id => async dispatch => {
+	try {
+		dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-        dispatch({ 
-            type: PRODUCT_LIST_SUCCESS,
-            payload: data
-        })
+		const { data } = await axios.get(`/api/products/${id}`);
 
-    }catch(err){
-        dispatch({ 
-            type: PRODUCT_LIST_FAIL,
-            payload: err.response && err.response.data.message ? err.response.data.message : err.message
-            
-        })
-    }
-}
+		dispatch({
+			type: PRODUCT_DETAILS_SUCCESS,
+			payload: data,
+		});
+	} catch (err) {
+		dispatch({
+			type: PRODUCT_DETAILS_FAIL,
+			payload:
+				err.response && err.response.data.message
+					? err.response.data.message
+					: err.message,
+		});
+	}
+};
 
-
-export const listProductDetails =  (id) => async(dispatch) => {
-    try{
-        dispatch({ type: PRODUCT_DETAILS_REQUEST})
-
-
-        const {data} = await axios.get(`/api/products/${id}`)
-
-        dispatch({ 
-            type: PRODUCT_DETAILS_SUCCESS,
-            payload: data
-        })
-
-    }catch(err){
-        dispatch({ 
-            type: PRODUCT_DETAILS_FAIL,
-            payload: err.response && err.response.data.message ? err.response.data.message : err.message
-            
-        })
-    }
-}
-
-export const deleteProduct = (id) => async (dispatch, getState) => {
+export const deleteProduct = id => async (dispatch, getState) => {
 	try {
 		dispatch({
 			type: PRODUCT_DELETE_REQUEST,
@@ -67,6 +78,38 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
 	} catch (err) {
 		dispatch({
 			type: PRODUCT_DELETE_FAIL,
+			payload:
+				err.response && err.response.data.message
+					? err.response.data.message
+					: err.message,
+		});
+	}
+};
+
+export const createProduct = () => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: PRODUCT_CREATE_REQUEST,
+		});
+
+		const { userLogin: { userInfo } } = getState();
+
+		const config = {
+			headers: {
+                'Content-Type': 'application/json',
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		};
+
+		const { data } = await axios.post(`/api/products`, {}, config);
+
+		dispatch({
+            type: PRODUCT_CREATE_SUCCESS,
+            payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: PRODUCT_CREATE_FAIL,
 			payload:
 				err.response && err.response.data.message
 					? err.response.data.message
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -3,8 +3,9 @@ import { Table, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { deleteProduct, listProducts } from "../actions/productActions";
+import { deleteProduct, listProducts, createProduct } from "../actions/productActions";
 import { LinkContainer } from "react-router-bootstrap";
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 const ProductListScreen = ({ history, match }) => {
     const dispatch = useDispatch()
@@ -18,17 +19,28 @@ const ProductListScreen = ({ history, match }) => {
     const productDelete = useSelector(state => state.productDelete)
     const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete
 
+    const productCreate = useSelector(state => state.productCreate)
+    const { loading: loadingCreate, error: errorCreate, success: successCreate, product: createdProduct } = productCreate
+
     useEffect(() => {
-        if(userInfo && userInfo.isAdmin){
-            dispatch(listProducts())
-        } else {
+        dispatch({ 
+            type: PRODUCT_CREATE_RESET
+        })
+
+        if(!userInfo.isAdmin){
             history.push('/login')
         }
 
-    }, [dispatch, history, userInfo, successDelete])
+        if(successCreate){
+            history.push(`/admin/product/${createdProduct._id}/edit`)
+        }else{
+            dispatch(listProducts())
+        }
+
+    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
 
     const createProductHandler = () => {
-        console.log('Product created')
+        dispatch(createProduct())
     }
 
     const deleteHandler = (id) => {
@@ -52,7 +64,10 @@ const ProductListScreen = ({ history, match }) => {
 
         {loadingDelete && <Loader/>}
         {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
-        {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+        {loadingCreate && <Loader/>}
+        {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
+        {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : 
+        (
             <Table striped bordered hover responsive className='table-sm'>
                 <thead>
                     <tr>
