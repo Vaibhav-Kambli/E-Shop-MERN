a0ea1865531e3f0fa4eb94ecc31366b42f2c90af
Vaibhav Kambli <vaibhavk2608@gmail.com>
2020-12-19 15:34:41 -0500

    Updated screen code

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -25,13 +25,15 @@ const HomeScreen = () => {
 	return (
 		<>
 			<h1>Latest Products</h1>
-			{loading ? (<Loader/>) : error ? (<Message variant="danger">{error}</Message>) : <Row>
+			{loading ? (<Loader/>) : error ? (<Message variant="danger">{error}</Message>) : ( 
+				<>
+				<Row>
 				{products.map((product) => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+					<Col className='align-items-stretch d-flex' key={product._id} sm={12} md={6} lg={4} xl={3}>
 						<Product product={product} />
 					</Col>
 				))}
-			</Row>}
+			</Row> </>)}
 
 			
 		</>
